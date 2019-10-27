91c092ddb89136499811175c4a2856b619fd1a56
Tri Pham <thientri399@gmail.com>
2019-10-26 22:49:37 -0500

    initial commit for exercise 3

---
+++ CreatePosts.php
@@ -0,0 +1,35 @@
+<!--Reference https://wiki.ittc.ku.edu/ittc_wiki/index.php/EECS448:Lab5-->
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "thientri399", "Utiew3Un", "thientri399");
+
+  $username = $_POST["username"];
+  $post = $_POST["post"];
+
+  if($mysqli->connect_error){
+    printf("Connection failed: " . $mysqli->connect_error);
+    exit();
+  }
+
+  $query = "SELECT user_id FROM Users";
+
+  if($result = $mysqli->query($query)){
+    while ($row = $result->fetch_assoc()){
+      if($username != $row["user_id"]){
+        $post = "";
+        echo "The post was not written by an existing user <br>";
+      }
+      else{
+        $insert = "INSERT INTO Posts (content,author_id) VALUES ('$post','$username')";
+        $mysqli->query($insert);
+        echo "The post was saved successfully <br>";
+      }
+    }
+    $result->free();
+  }
+
+  echo "Connected successfully";
+  $mysqli->close();
+?>
