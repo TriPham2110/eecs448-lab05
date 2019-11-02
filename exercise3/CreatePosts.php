1202679baa7a9527463765bf202a21821bba5438
TriPham2110 <TriPham2110@users.noreply.github.com>
2019-11-01 21:46:57 -0500

    Finished exercise 6

--- exercise3/CreatePosts.php
+++ exercise3/CreatePosts.php
@@ -7,7 +7,7 @@
 
   $username = $_POST["username"];
   $post = $_POST["post"];
-  $userExists = false;
+  $userExists = true;
 
   if($mysqli->connect_error){
     printf("Connection failed: " . $mysqli->connect_error);
@@ -19,20 +19,22 @@
   if($result = $mysqli->query($query)){
     while ($row = $result->fetch_assoc()){
       if($username != $row["user_id"]){
-        $post = "";
-        $userExists = true;
+        $userExists = false;
       }
       else{
         $insert = "INSERT INTO Posts (content,author_id) VALUES ('$post','$username')";
         $mysqli->query($insert);
-        echo "The post was saved successfully <br>";
+        $userExists = true;
       }
     }
     $result->free();
   }
 
-  if($userExists){
+  if(!$userExists){
     echo "The post was not written by an existing user <br>";
+  }
+  else{
+    echo "The post was saved successfully <br>";
   }
 
   echo "Connected successfully";
--- exercise6/ViewUsersPosts.php
+++ exercise6/ViewUsersPosts.php
@@ -2,6 +2,8 @@
   error_reporting(E_ALL);
   ini_set("display_errors", 1);
 
+  $username = $_POST['userID'];
+
   $mysqli = new mysqli("mysql.eecs.ku.edu", "thientri399", "Utiew3Un", "thientri399");
 
   if($mysqli->connect_error){
@@ -9,17 +11,17 @@
     exit();
   }
 
-  echo "<table border=1 width=20% style='margin: 0px auto; margin-top: 150px; text-align: center; background-color: #939393;'>";
+  echo "<table border=1 width=25% style='margin: 0px auto; margin-top: 150px; text-align: center; background-color: #939393;'>";
   echo "<tr>";
-  echo "<th>Usernames fetched from database</th>";
+  echo "<th colspan=3>Posts info fetched from database for user $username</th>";
   echo "</tr>";
 
-  $query = "SELECT user_id FROM Users";
+  $query = "SELECT content FROM Posts WHERE author_id = '$username'";
 
   if($result = $mysqli->query($query)){
     while ($row = $result->fetch_assoc()){
       echo "<tr>";
-      echo "<th style= 'background-color: #e0e0e0;'>" . $row["user_id"] . "</th>";
+      echo "<th style= 'background-color: #e0e0e0;'>" . $row["content"] . "</th>";
       echo "</tr>";
     }
     $result->free();
